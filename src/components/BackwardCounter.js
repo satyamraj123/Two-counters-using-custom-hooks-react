40df152bd63ed132c864d7fc3250fe29b8b50c04
satyamraj <satyamraj-creator@users.noreply.github.com>
2022-03-25 18:14:44 +0530

    creating custom hook

--- src/components/BackwardCounter.js
+++ src/components/BackwardCounter.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react';
+import useCounter from '../hooks/use-counter';
 
 import Card from './Card';
 
 const BackwardCounter = () => {
+  
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
